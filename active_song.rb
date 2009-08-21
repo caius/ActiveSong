4a2bb0eb4b6b33ec22bc023c1dbf11c522acbe88
Caius Durling <dev@caius.name>
2009-08-21 19:04:18 +0100

    Initial commit of ActiveSong

---
+++ active_song.rb
@@ -0,0 +1,37 @@
+require "rubygems"
+require "httparty"
+
+class ActiveSong
+  class Result < String
+    def play!
+      `say "#{self}"`
+    end
+  end
+
+
+  include HTTParty
+
+  API_KEY = "d406082d09a06db68-temporary.API.access"
+
+  base_uri "http://lyricsfly.com/api/api.php"
+  default_params :i => API_KEY
+
+  def self.find params = {}
+    params[:artist] ||= "%"
+    params[:track] ||= "%"
+    
+    # Grab the right bit of the response
+    result = get("", :query => {:a => encode_string(params[:artist]), :t => encode_string(params[:track])})["start"]["sg"].first["tx"]
+    # Sort it out into a proper format
+    result.gsub!("[br]", "\n").gsub!(/\n+/, "\n")
+    # Grab the first 4 lines and return it as an ActiveSong::Result
+    Result.new(result.split("\n")[0..3].join("\n"))
+  end
+
+protected
+
+  def self.encode_string str
+    str.gsub(/[^\w]/i, "%")
+  end
+
+end
